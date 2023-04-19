bc6b25e4a877868a8738c9330aaa4e67230f8397
YanPavlukovich <filin022@gmail.com>
2023-04-19 20:57:04 +0300

    прописал слайсы через декомпозицию, один редьюсер, набор экшенов и начальное состояние

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
-import  moviesReducer  from './movies-slice';
-import  searchReducer  from './search-slice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import moviesReducer from './slices/movies-slice';
+import errorReducer from './slices/error-slice';
+import ratedReducer from './slices/rated-slice';
+import genresReducer from './slices/genres-slice';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 
 const rootReducer = combineReducers({
   movies: moviesReducer,
-  search: searchReducer,
+  error: errorReducer,
+  rated: ratedReducer,
+  genres: genresReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
+export default store;
+
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
